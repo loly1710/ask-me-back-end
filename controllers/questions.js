b3c74efed464c2df4d5739320fc8d3f89ca6403c
fatemnhm <fatemanhm@gmail.com>
2024-11-21 03:15:42 +0300

    notfinal

--- controllers/questions.js
+++ controllers/questions.js
@@ -36,7 +36,16 @@ router.get('/', async(req, res) => {
 // SHOW - GET ONE QUESTION
 router.get('/:questionId', async(req,res) => {
     try {
-         const question = await Question.findById(req.params.questionId).populate('author');
+         const question = await Question.findById(req.params.questionId)
+                                .populate('author')
+                                .populate({
+                                    path: 'comments.author',
+                                    model: 'User'
+                                })
+                                .populate({
+                                    path: 'comments.replies.author',
+                                    model: 'User'
+                                });
          res.status(200).json(question);
     } catch (error) {
         res.status(500).json(error);
@@ -131,6 +140,81 @@ router.delete('/:questionId/comments/:commentId', async (req, res) => {
     }
 })
 
+//  REPLY TO A COMMENT
+router.post('/:questionId/comments/:commentId/reply', async (req, res) => {
+    try {
+      req.body.author = req.user._id;
+  
+      const question = await Question.findById(req.params.questionId);
+        if (!question) {
+            return res.status(404).json({ message: 'Question not found'   
+        });
+      }
+  
+      const commentIndex = question.comments.findIndex(el => el.id === req.params.commentId);
+      if (commentIndex === -1) {
+        return res.status(404).json({ message: 'Comment not found' });
+      }
+  
+      question.comments[commentIndex].replies.push(req.body);
+  
+      await question.save();
 
+      const fetch_question = await Question.findById(req.params.questionId)
+                                .populate('author')
+                                .populate({
+                                    path: 'comments.author',
+                                    model: 'User'
+                                })
+                                .populate({
+                                    path: 'comments.replies.author',
+                                    model: 'User'
+                                });
+
+      const newReply = fetch_question.comments[commentIndex].replies[question.comments[commentIndex].replies.length - 1]
+  
+      res.status(201).json({ message: 'Reply saved successfully', newReply: newReply }); // Send a success message
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' }); // Generic error message for now
+    }
+});
+
+// UPDATE A COMMENT REPLY
+router.put('/:questionId/comments/:commentId/reply/:replyId', async (req, res) => {
+    try {
+        
+      const question = await Question.findById(req.params.questionId);
+      const comment = question.comments.id(req.params.commentId);
+      const reply = comment.replies.id(req.params.replyId)
+      reply.text = req.body.text;
+      await question.save();
+      res.status(200).json({ message: 'Ok' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
+// DELETE A COMMENT REPLY
+router.delete('/:questionId/comments/:commentId/reply/:replyId', async (req, res) => {
+    try {
+        const question = await Question.findById(req.params.questionId);
+            if (!question) {
+                return res.status(404).json({ message: 'Question not found'   
+            });
+        }
+  
+        const commentIndex = question.comments.findIndex(el => el.id === req.params.commentId);
+        if (commentIndex === -1) {
+            return res.status(404).json({ message: 'Comment not found' });
+        }
+  
+      question.comments[commentIndex].replies.remove({ _id: req.params.replyId })
+      await question.save()
+      res.status(200).json({ message: "Ok" })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 module.exports = router
